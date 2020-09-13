f748afbd98ab7c41b1dc0d056bb0aa793c3b3729
Denys Korotin <denisk@strivtech.com>
2020-09-14 00:53:27 +0300

    Added request to server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = new express();
+const https = require('https');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +9,10 @@ app.use(express.static(path.join(__dirname + '/www')));
 let handler = (req, res) => res.sendFile(path.join(__dirname + '/www/index.html'));
 
 app.get('0', handler);
+
+setInterval(function () {
+  https.get(PROD_URL);
+}, 1200000);
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
