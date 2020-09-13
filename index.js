fedae1f4a635a646b41a2561afe7d18126494107
Denys Korotin <denisk@strivtech.com>
2020-09-14 00:49:14 +0300

    Added port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = new express();
 const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname + '/www')));
 
@@ -8,6 +9,6 @@ let handler = (req, res) => res.sendFile(path.join(__dirname + '/www/index.html'
 
 app.get('0', handler);
 
-app.listen(process.env.PORT | 8080, () => {
-  console.log(`App listening`);
+app.listen(PORT, () => {
+  console.log(`App listening on ${PORT}`);
 });
